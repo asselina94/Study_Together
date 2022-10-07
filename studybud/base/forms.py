82d756092f5b94276a0f9ccab2dcc66b0d347182
Assel Batu <batuassel@gmail.com>
2022-10-07 12:08:29 +0200

    Edited User Account page

--- studybud/base/forms.py
+++ studybud/base/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from .models import Room
+from django.contrib.auth.models import User
 
 class RoomForm(ModelForm):
     class Meta:
@@ -7,3 +8,7 @@ class RoomForm(ModelForm):
         fields = '__all__'
         exclude = ['host','participants']
 
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username','email']
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -10,6 +10,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room,Topic,Message
 from .forms import RoomForm
+from .forms import RoomForm, UserForm
 
 # Create your views here.
 
@@ -178,3 +179,15 @@ def deleteMessage(request,pk):
         
         return redirect('home')
     return render(request, 'base/delete.html',{'obj': message})
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=request.user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile',pk=user.id)
+    return render(request,'base/update-user.html',{'form':form})
