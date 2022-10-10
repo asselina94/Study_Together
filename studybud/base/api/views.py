3d12587ba926c1573ad6cc8e12ba5a5eb7291b2d
Assel Batu <batuassel@gmail.com>
2022-10-10 22:04:47 +0200

    made changes in urls, views

--- studybud/base/api/views.py
+++ studybud/base/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from base.models import Room
+from .serializers import RoomSerializer
 
 
 @api_view(['GET'])
@@ -11,9 +12,18 @@ def getRoutes(request):
         'GET /api/rooms/:id'
     ]
     return Response(routes)
-    
+
 
 @api_view(['GET'])
 def getRooms(request):
     rooms = Room.objects.all() 
-    return Response(rooms)
+    serializer = RoomSerializer(rooms, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getRoom(request,pk):
+    room = Room.objects.get(id=pk) 
+    serializer = RoomSerializer(room, many=False)
+    return Response(serializer.data)
+    
--- studybud/studybud/settings.py
+++ studybud/studybud/settings.py
@@ -40,16 +40,22 @@ INSTALLED_APPS = [
     'base.apps.BaseConfig',
     
     'rest_framework',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+     "corsheaders.middleware.CorsMiddleware",
+    
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'studybud.urls'
@@ -132,3 +138,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOW_ALL_ORIGINS = True
